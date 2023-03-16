b6801cfa4fa9a7a180e26f90ba17068400e8ef4e
Thiagohnc <thiagohnc@hotmail.com>
2023-03-16 20:21:11 -0300

    feat: automating analysis for all models

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -1,6 +1,7 @@
 import sys
-from file_reader import read_file
+from file_reader import list_files_in_folder, read_file
 import matplotlib.pyplot as plt
+import numpy as np
 
 def normalize_data(all_data):
     total_population = sum(all_data[0])
@@ -11,19 +12,88 @@ def normalize_data(all_data):
         infecteds.append(data_from_time_t[2] / total_population)
         removeds.append(data_from_time_t[3] / total_population)
 
-    return t, susceptibles, infecteds, removeds
+    return np.array(t), np.array(susceptibles), np.array(infecteds), np.array(removeds)
+
+
+def get_statistics(folder):
+    input_files = list_files_in_folder(folder)
+    list_t, list_susceptibles, list_infecteds, list_removeds = [], [], [], []
+    for input_file in input_files:
+        data = read_file(input_file)
+        t, susceptibles, infecteds, removeds = normalize_data(data)
+        list_t.append(t)
+        list_susceptibles.append(susceptibles)
+        list_infecteds.append(infecteds)
+        list_removeds.append(removeds)
+
+    mean_t = sum(list_t)/len(input_files)
+    mean_susceptibles = sum(list_susceptibles)/len(input_files)
+    mean_infecteds = sum(list_infecteds)/len(input_files)
+    mean_removeds = sum(list_removeds)/len(input_files)
+
+    return mean_t, mean_susceptibles, mean_infecteds, mean_removeds
+
 
 if __name__ == '__main__':
     args = sys.argv[1:]
-    filepath = args[0]
-    title = ' '.join(args[1:])
 
-    data = read_file(filepath)
-    t, susceptibles, infecteds, removeds = normalize_data(data)
+    plot_path = f'..\\plots\\{args[0]}'
+    suptitle = ' '.join(args[1].split('_'))
+    folders = []
+    titles = []
 
-    plt.plot(t, susceptibles, label="suscetível", color='green')
-    plt.plot(t, infecteds, label="infectado", color='red')
-    plt.plot(t, removeds, label="removido", color='orange')
+    rows = len(args[2:])//2
+
+    model_tags = ['gnp', 'ba', 'ws']
+    model_names = ['G(n,p)', 'BA', 'WS']
+    for i in range(rows):
+        folders.append([])
+        titles.append([])
+        for j in range(3):
+            folders[i].append(f'..\\output\\{model_tags[j]}\\{args[2*i+2]}')
+            titles[i].append(model_names[j] + ' - ' + ' '.join(args[2*i+3].split('_')))
+
+    fig, axs = plt.subplots(len(args[2:])//2, 3)
+    for i in range(rows):
+        for j in range(3):
+            t, susceptibles, infecteds, removeds = get_statistics(folders[i][j])
+            ax = axs[i, j] if len(args[1:])//2 > 1 else axs[j]
+            ax.plot(t, susceptibles, '--', label="suscetível", color='green')
+            ax.plot(t, infecteds, '-', label="infectado", color='red')
+            ax.plot(t, removeds, ':', label="removido", color='orange')
+            ax.set_title(titles[i][j])
+
+    fig.suptitle(suptitle)
+    fig.set_figheight(rows * 3)
+    fig.set_figwidth(9)
+    fig.tight_layout()
+    plt.savefig(plot_path)
+
+'''
+    folder = args[0]
+    plot_filepath = args[1]
+    title = ' '.join(args[2:])
+
+    input_files = list_files_in_folder(folder)
+    list_t, list_susceptibles, list_infecteds, list_removeds = [], [], [], []
+    for input_file in input_files:
+        data = read_file(input_file)
+        t, susceptibles, infecteds, removeds = normalize_data(data)
+        list_t.append(t)
+        list_susceptibles.append(susceptibles)
+        list_infecteds.append(infecteds)
+        list_removeds.append(removeds)
+
+    mean_t = sum(list_t)/len(input_files)
+    mean_susceptibles = sum(list_susceptibles)/len(input_files)
+    mean_infecteds = sum(list_infecteds)/len(input_files)
+    mean_removeds = sum(list_removeds)/len(input_files)
+
+    plt.plot(mean_t, mean_susceptibles, '--', label="suscetível", color='green')
+    plt.plot(mean_t, mean_infecteds, '-', label="infectado", color='red')
+    plt.plot(mean_t, mean_removeds, ':', label="removido", color='orange')
     plt.legend()
     plt.title(title)
-    plt.show()
+    plt.savefig(plot_filepath)
+
+'''
--- analysis/file_reader.py
+++ analysis/file_reader.py
@@ -1,3 +1,5 @@
+import os
+
 def read_file(filepath):
     result = []
     with open(filepath, "r") as file:
@@ -5,4 +7,11 @@ def read_file(filepath):
         for line in lines:
             line = line.strip().split(' ')
             result.append([int(line[0]), int(line[1]), int(line[2]), int(line[3])])
+    return result
+
+def list_files_in_folder(folder):
+    result = []
+    files = os.listdir(folder)
+    for file in files:
+        result.append(os.path.join(folder, file))
     return result
